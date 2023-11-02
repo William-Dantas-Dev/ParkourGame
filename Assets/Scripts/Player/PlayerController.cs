1edf51011ead08c497cf9bcf1d195d35012afc8b
William-Dantas-Dev <shiroe.gaming.dev@gmail.com>
2023-11-02 19:38:30 -0300

    animations players

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,19 +7,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 500f;
     CameraController cameraController;
+    private Animator animator;
 
     private Quaternion targetRotation;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
     }
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        float moveAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
 
         var moveInput = (new Vector3(horizontal, 0f, vertical)).normalized;
         var moveDir = cameraController.PlanarRotation * moveInput;
@@ -31,5 +33,6 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
     }
 }
