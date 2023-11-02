1f46f9b0c3e5d7cc0bd2e7800b8aec7ccb2e38e1
William-Dantas-Dev <shiroe.gaming.dev@gmail.com>
2023-11-02 20:50:51 -0300

    change to Character Controller and add gravity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 500f;
+    [Header("References")]
     CameraController cameraController;
     private Animator animator;
+    private CharacterController characterController;
 
+    [Header("Move Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Gravity Settings")]
+    private float ySpeed;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Vector3 groundCheckOffset;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+
+    [Header("Rotation Settings")]
+    [SerializeField] private float rotationSpeed = 500f;
     private Quaternion targetRotation;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
         animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
     private void Update()
     {
@@ -26,13 +41,36 @@ public class PlayerController : MonoBehaviour
         var moveInput = (new Vector3(horizontal, 0f, vertical)).normalized;
         var moveDir = cameraController.PlanarRotation * moveInput;
 
-        if(moveAmount > 0)
+        GroundCheck();
+        if (isGrounded)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            ySpeed = -0.5f;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+        var velocity = moveDir * moveSpeed;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+        if (moveAmount > 0)
+        {
+            //transform.position += moveDir * moveSpeed * Time.deltaTime;
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
